92ac713b02fef143ae53767f219edda88a98f2d3
kouzber123 <tom.nguyen@windowslive.com>
2026-06-27 13:17:30 +0300

    feat(common): add job class factory for Json data

--- Scripts/Entities/Base/JobClass.cs
+++ Scripts/Entities/Base/JobClass.cs
@@ -1,14 +1,17 @@
 using System;
+using startup.Scripts.Entities.Common;
 
-
-public class JobClass
+namespace startup.Scripts.Entities.Base
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Type { get; set; }
-    public string Description { get; set; }
-    public int Mana { get; set; } = 2;
-    public int Strength { get; set; } = 2;
-    public int Agility { get; set; } = 2;
-    public int Stamina { get; set; } = 2;
+    public class JobClass
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public JobClassTypes Type { get; set; }
+        public string Description { get; set; }
+        public int Mana { get; set; } = 2;
+        public int Strength { get; set; } = 2;
+        public int Agility { get; set; } = 2;
+        public int Stamina { get; set; } = 2;
+    }
 }
---
+++ Scripts/Entities/Common/JobClassTypes.cs
@@ -0,0 +1,22 @@
+namespace startup.Scripts.Entities.Common
+{
+    public enum JobClassTypes
+    {
+        Warrior,
+        Rogue,
+        Mage,
+        Archer,
+        Cleric,
+        Paladin,
+        Necromancer,
+        Druid,
+        Bard,
+        Monk,
+        Assassin,
+        Sorcerer,
+        Warlock,
+        Ranger,
+        Shaman,
+        Knight,
+    }
+}
